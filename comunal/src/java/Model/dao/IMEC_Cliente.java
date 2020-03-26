40376ba30c6990b34a80345bd0cbbeb39aae5df5
DiegoPintoGomez <44154154+DiegoPintoGomez@users.noreply.github.com>
2020-03-25 23:29:13 -0600

    cliente

--- comunal/src/java/Model/dao/IMEC_Cliente.java
+++ comunal/src/java/Model/dao/IMEC_Cliente.java
@@ -4,7 +4,7 @@ public enum IMEC_Cliente {
     INSERTAR("INSERT INTO cliente (id_cliente, usuario_id_usuario, apellidos, nombre, telefono) VALUES (?, ?, ?, ?, ?)"),
     //MODIFICAR("UPDATE estudiante SET apellidos=?, nombre=? WHERE id=?; "),
     EXCLUIR("DELETE FROM cliente WHERE id_cliente=?; "),
-    CONSULTAR("SELECT * FROM cliente WHERE id_usuario=?; "),
+    CONSULTAR("SELECT * FROM cliente WHERE id_cliente=?; "),
     LISTAR("SELECT * FROM cliente ; ");
 
     IMEC_Cliente(String comando) {
